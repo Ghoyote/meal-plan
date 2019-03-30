f8e3abf0d6e6f7f209594bc46e6c0b264318db6a
harrietfiagbor <harrietfiagbor@gmail.com>
2019-03-30 14:01:00 +0000

    created post request for adding users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+var User = require('../models/user');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/create', function(req, res, next) {
+  var newUser = new User({
+    name: {
+      first: req.body.firstName,
+      last: req.body.lastName,
+      middle: req.body.middleName
+    },
+    school_id: req.body.schoolId,
+    subscription_type: req.body.subscription
+  });
+
+  newUser.save(function(err, result) {
+    if (err) return res.json(err);
+    res.json(result);
+  });
 });
 
 module.exports = router;
